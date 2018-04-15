42e02f4b12f0572ed8bff91df35289e697b3fa6d
John Cummings <jcummings5@gmail.com>
2018-04-15 18:46:58 -0400

    lol

--- js/issues.js
+++ js/issues.js
@@ -86,7 +86,8 @@ function initTable() {
         console.log(name, args);
     });
     $table.on('editable-save.bs.table', function(field, row, oldValue, $el) {
-        console.log(field, row, oldValue, $el);
+        //console.log(field, row, oldValue, $el);
+        console.log(row);
 
         var url = 'http://ec2-52-91-175-30.compute-1.amazonaws.com/api/addIssue'+ row.iss_id;
         axios.post(url, {
