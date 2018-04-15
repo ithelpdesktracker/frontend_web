df407ee0bcf654484d5658a2a76b88df13e6c720
John Cummings <jcummings5@gmail.com>
2018-04-15 17:19:33 -0400

    lol

--- js/issues.js
+++ js/issues.js
@@ -62,7 +62,7 @@ function initTable() {
     });
     // sometimes footer render error.
     setTimeout(function () {
-        $table.bootstrapTable('resetView');
+        $table.bootstrapTable('refresh');
     }, 200);
     $table.on('check.bs.table uncheck.bs.table ' +
         'check-all.bs.table uncheck-all.bs.table',
@@ -119,43 +119,7 @@ function detailFormatter(index, row) {
     return html.join('');
 }
 
-function operateFormatter(value, row, index) {
-    return [
-        '<a class="like" href="javascript:void(0)" title="Like">',
-        '<i class="glyphicon glyphicon-heart"></i>',
-        '</a>  ',
-        '<a class="remove" href="javascript:void(0)" title="Remove">',
-        '<i class="glyphicon glyphicon-remove"></i>',
-        '</a>'
-    ].join('');
-}
-window.operateEvents = {
-    'click .like': function (e, value, row, index) {
-        alert('You click like action, row: ' + JSON.stringify(row));
-    },
-    'click .remove': function (e, value, row, index) {
-        $table.bootstrapTable('remove', {
-            field: 'id',
-            values: [row.id]
-        });
-    }
-};
 
-function totalTextFormatter(data) {
-    return 'Total';
-}
-
-function totalNameFormatter(data) {
-    return data.length;
-}
-
-function totalPriceFormatter(data) {
-    var total = 0;
-    $.each(data, function (i, row) {
-        total += +(row.price.substring(1));
-    });
-    return '$' + total;
-}
 
 function getHeight() {
     return $(window).height() - $('h1').outerHeight(true);
